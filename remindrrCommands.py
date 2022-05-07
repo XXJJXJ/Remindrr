65e824b7448909f2d2662a4e758b7229bb1093be
XXJJXJ <yexinjian1506@gmail.com>
2022-05-07 16:19:16 +0800

    Add remindrrCommands.py skeleton

---
+++ remindrrCommands.py
@@ -0,0 +1,16 @@
+import os
+import datetime
+
+# use firebase for data storage
+def addTask(user, taskName, deadline, priority):
+    print("Developing")
+
+def myTask(user):
+    print("Getting user data")
+
+def deleteTask(user, taskName):
+    print("Deleting Task lololol")
+
+def setReminder(user, reminderTime):
+    print(f"Reminder set to: {reminderTime}")
+
