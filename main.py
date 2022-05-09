09c05562f5083edbce0452092f7a97e477bc4a48
XXJJXJ <yexinjian1506@gmail.com>
2022-05-09 21:59:25 +0800

    Update main.py: For basic deployment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import remindrrCommands as rmdr
+import sys
 
 TOKEN = open('Token', "r").read() # to be hidden before pushing
 
@@ -29,43 +30,52 @@ async def on_message(message):
     if message.channel.name == "general":
         if user_message.lower() == 'hello':
             await message.channel.send(f'Hello {username}!')
-            return
+
         elif user_message.lower() == 'bye':
             await message.channel.send(f'See ya later {username}!')
-            return
-        elif user_message.lower() == "help":
-            await message.channel.send('Help function under development')
-            return
 
-        elif user_message.startswith("addTask "):
-            # can modify to account for duplicates and give a response without
-            #
+        elif user_message.lower() == "help":
+            await message.channel.send(rmdr.HELP)
+
+        elif user_message.startswith("addTask"):
             try:
-                components = user_message.split(" ")
-                priority = components[3]
-                date = components[2]
-                taskName = components[1]
-                msg = rmdr.addTask(str(message.author), taskName, date, priority)
+                components = user_message.split(",")
+                date = components[1].strip()
+                taskName = components[0].split(" ", 1)[1].strip()
+                msg = rmdr.addTask(str(message.author), taskName, date)
                 # get message
                 await message.channel.send(msg)
             except:
-                await message.channel.send('''Format for addTask command is: E.g.\n\n!addTask taskname ddmmyyyy priority(1 for highest, larger numbers for lower priority)''')
+                print("Oops!", sys.exc_info()[0], "occurred.") # for debugging
+                await message.channel.send(
+                    f"Wrong input format for addTask! :woozy_face:\n"
+                    f"Format for addTask command is:\n\n"
+                    f"!addTask taskname, dd/mm/yyyy"
+                )
 
         elif user_message.startswith("myTasks"):
             await message.channel.send(rmdr.myTask(username))
 
-        elif user_message.startswith("deleteTask "):
+        elif user_message.startswith("deleteTask"):
             # delete by name easy --> next time add a delete by index
             position = user_message.find(" ") #find first space and the rest are the name
-            # TODO: Might want to do some error handling here
-            taskName = user_message[position + 1:]
-            await message.channel.send(rmdr.deleteTask(username, taskName))
+            if position < 0:
+                await message.channel.send("Correct format:\n\n!deleteTask taskname :woozy_face:")
+            else:
+                taskName = user_message[position + 1:]
+                await message.channel.send(rmdr.deleteTask(username, taskName))
 
         elif user_message.startswith("setReminder "):
             await message.channel.send('setReminder Function under development')
 
         elif user_message.startswith("timeNow"):
             await message.channel.send(rmdr.getTime())
+
+        elif user_message.startswith("setTimer "):
+            # time in seconds
+            components = user_message.split(" ")
+            time = int(components[1])
+            await message.channel.send(await rmdr.setTimeout(username, time))
 
         else:
             await message.channel.send("Sorry but I don't understand what you want! :sweat_smile:")
