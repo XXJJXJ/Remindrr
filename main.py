3f1cc432d701ff35ad62591671badadfec54ac9f
XXJJXJ <yexinjian1506@gmail.com>
2022-05-11 23:17:56 +0800

    Fix minor bugs in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import sys
 
 import GroupMessage
 import PrivateMessage
@@ -15,23 +16,29 @@ async def on_message(message):
     if message.author == client.user:
         return
     username = str(message.author)
-    user_message = str(message.content).split(" ", 1)
-    command = user_message[0].lower()
-    args = user_message[1]
+    user_message = str(message.content)
 
     if not user_message.startswith("!"):
         return
     else:
         user_message = user_message[1:]
 
+    user_message = user_message.split(" ", 1)
+    command = user_message[0].lower()
+    if len(user_message) == 2:
+        args = user_message[1]
+    else:
+        args = ""
+
     try:
         channel = str(message.channel.name) # will cause error if receive dm
-        group_name = message.channel.guild
+        group_name = str(message.channel.guild)
         # log in the console
         print(f'{username}: {user_message} ({group_name} : {channel})')
         await GroupMessage.handleMessage(username, group_name, command, args, message)
     except:
         # deal with DM functions
+        print("Oops!", sys.exc_info()[0], sys.exc_info()[1], "occurred.")  # for debugging
         print(f"DM received from {username} : {user_message}")
         await PrivateMessage.handleMessage(username, command, args, message)
 
